d6214d8fa995d51f26b75bb10d031cde4853b2f0
Minjae Lee <mmj9808@gmail.com>
2022-10-08 17:07:39 +0900

    FEATURE: BST 삭제 기능 추가
    BST 단순 삭제 기능을 추가했습니다.

--- main.cpp
+++ main.cpp
@@ -2,59 +2,202 @@
 
 using namespace std;
 
-struct Node{
-    int data;
-    Node* left;
-    Node* right;
-    Node(int _data, Node* _left, Node* _right): data(_data), left(_left), right(_right){}
+struct Node {
+    int key;
+    int height;
+    Node *left;
+    Node *right;
+
+    Node() : key(0), height(0), left(nullptr), right(nullptr) {}
 };
 
-void insert(Node** root, int value){
-    //삽입 위치 찾기
-    Node *p = *root, *q = nullptr;
 
-    while(p!= nullptr){
+Node *getNode() {
+    return new Node();
+}
+
+
+void insertBST(Node **Tree, int newKey) {
+    //삽입 위치 찾기
+    Node *p = *Tree, *q = nullptr;
+
+    while (p != nullptr) {
         q = p;
-        if(value < p->data) p = p->left;
-        else if(value > p->data) p = p->right;
-        else return;
+        if (newKey < p->key) p = p->left;
+        else if (newKey > p->key) p = p->right;
+        else {
+            cout << "i " << newKey << " : The key already exist" << '\n';
+            return;
+        }
     }
 
     //새로운 노드 만들어서 삽입
-    Node *newNode = new Node(value, nullptr, nullptr);
+    Node *newNode = getNode();
+    newNode->key = newKey;
+    newNode->right = nullptr;
+    newNode->left = nullptr;
 
-    if(q == nullptr){
-        *root = newNode;
-    }
-    else if(value < q->data){
+    if (*Tree == nullptr) {
+        *Tree = newNode;
+    } else if (newKey < q->key) {
         q->left = newNode;
-    }
-    else{
+    } else {
         q->right = newNode;
     }
 }
 
+int height(Node *Tree) {
 
-void inOrder(Node* root){
-    if(root == nullptr) return;
-    inOrder(root->left);
-    cout << root->data<<'\n';
-    inOrder(root->right);
+}
+
+bool noNodes(Node *Tree) {
+
+}
+
+Node *maxNode(Node *Tree) {
+    Node *p = Tree;
+
+    while (p->right) {
+        p = p->right;
+    }
+
+    return p;
+}
+
+Node *minNode(Node *Tree) {
+
+}
+
+
+void deleteBST(Node **Tree, int deleteKey) {
+    //삭제할 노드 p,  삭제할 노드의 부모 노드 q
+    Node *p = *Tree, *q = nullptr;
+
+    while (p != nullptr) {
+        if (deleteKey == p->key) break;
+        q = p;
+
+        if (deleteKey < p->key) p = p->left;
+        else p = p->right;
+    }
+
+    //삭제할 원소가 없음
+    if (p == nullptr) {
+        cout << "d " << deleteKey << " : The key does not exist" << '\n';
+        return;
+    }
+    //루트 노드만 있는 경우, 루트를 삭제
+    if (q == nullptr) {
+        if (p->left == nullptr && p->left == nullptr) {
+            delete *Tree;
+            *Tree = nullptr;
+            return;
+        }
+    }
+
+    // 삭제할 노드의 차수가 0인 경우(리프 노드)
+    if (p->left == nullptr && p->right == nullptr) {
+        if (q->left == p) {
+            delete q->left;
+            q->left = nullptr;
+        } else {
+            delete q->right;
+            q->right = nullptr;
+        }
+    }// 삭제할 노드의 차수가 1인 경우
+    else if (p->left == nullptr || p->right == nullptr) {
+        //삭제 해야 할 노드가 루트 노드고 차수가 1개인 경우
+        if(q== nullptr){
+            if(p->left != nullptr){
+                Node* tmp = maxNode(p->left);
+                p->key = tmp->key;
+                deleteBST(&p->left, p->key);
+            }
+            else{
+                Node* tmp = maxNode(p->right);
+                p->key = tmp->key;
+                deleteBST(&p->right, p->key);
+            }
+        }// 삭제 해야 할 노드가 루트 노드가 아니고, 차수가 1개인 노드인 경우
+        else if (p->left != nullptr) {
+            if (q->left == p) {
+                q->left = p->left;
+
+                delete p;
+
+
+                p = nullptr;
+            } else {
+                q->right = p->left;
+
+                delete p;
+                p = nullptr;
+            }
+        }// p의 오른쪽 자식만 있는 경우
+        else{
+            if(q->left == p){
+                q->left = p->right;
+
+                delete p;
+                p = nullptr;
+            }
+            else{
+                q->right = p->right;
+
+                delete p;
+                p = nullptr;
+            }
+        }
+
+
+    } else { // 삭제할 노드의 차수가 2인 경우
+        Node *r = maxNode(p->left);
+        p->key = r->key;
+        deleteBST(&p->left, r->key);
+    }
+}
+
+
+void inOrder(Node *Tree) {
+    if (Tree == nullptr) return;
+    inOrder(Tree->left);
+    cout << Tree->key << '\n';
+    inOrder(Tree->right);
 }
 
 
 int main() {
-    Node* root = nullptr;
+    Node *Tree = nullptr;
 
 
-    insert(&root, 50);
-    insert(&root, 20);
-    insert(&root, 30);
-    insert(&root, 10);
-    insert(&root, 30);
-    insert(&root, 30);
+    insertBST(&Tree, 50);
+    insertBST(&Tree, 20);
+    insertBST(&Tree, 30);
+    insertBST(&Tree, 10);
+
+    inOrder(Tree);
+    cout << "-------\n";
 
 
-    inOrder(root);
+    deleteBST(&Tree, 50);
+    inOrder(Tree);
+    cout << "-------\n";
+
+
+    deleteBST(&Tree, 20);
+    inOrder(Tree);
+    cout << "-------\n";
+
+
+    deleteBST(&Tree, 30);
+    inOrder(Tree);
+    cout << "-------\n";
+
+
+
+    deleteBST(&Tree, 10);
+    inOrder(Tree);
+    cout << "-------\n";
+
     return 0;
 }
